c7d5e0c17bc9f188d2e17bf26aa634a71b265c52
Brandon Lucia <blucia@gmail.com>
2020-04-20 09:42:26 -0400

    added bin size computation and random graph generator

--- pb.c
+++ pb.c
@@ -7,8 +7,9 @@
 #include <string.h>
 
 #define V_NAME_LEN 8
-#define NUM_BINS 256
+#define NUM_BINS 8
 
+#define v2bin(x) (x % NUM_BINS)
 int bin_sz[NUM_BINS];
 int num_edges;
 char *init_el_file(char *f){
@@ -23,9 +24,10 @@ char *init_el_file(char *f){
   }
 
   fstat(fd,&stat);
-  num_edges = stat.st_size;
+  int sz = stat.st_size;
+  num_edges = sz / V_NAME_LEN / 2;
   char *el = 
-    mmap(NULL, num_edges, PROT_READ, 
+    mmap(NULL, sz, PROT_READ, 
                              MAP_PRIVATE /*| MAP_HUGETLB | MAP_HUGE_1GB*/, fd, 0);
   if( el == MAP_FAILED ){
     fprintf(stderr,"bad edgelist map attempt\n");
@@ -35,23 +37,29 @@ char *init_el_file(char *f){
 }
 
 void bin_info(char *el){
-  char *cur = el;
-  char src[V_NAME_LEN+1];
-  char dst[V_NAME_LEN+1];
-  for(int i = 0; i < num_edges / (V_NAME_LEN * 2); i++){ 
 
-    memcpy(src,cur,V_NAME_LEN);
-    src[V_NAME_LEN] = '\0';
-    printf("%s\n",src);
-    cur+=V_NAME_LEN;
+  unsigned long *cur = (unsigned long *)el;
+  for(int i = 0; i < num_edges; i++){ 
 
-    memcpy(dst,cur,V_NAME_LEN);
-    dst[V_NAME_LEN] = '\0';
-    printf("%s\n",dst);
-    cur+=V_NAME_LEN;
+    unsigned long src = *cur;
+    bin_sz[ v2bin(src) ] ++;    
+    cur++;
 
-    printf("%16lu %16lu\n", *((unsigned long*)src), *((unsigned long*)dst));
+    unsigned long dst = *cur;
+    bin_sz[ v2bin(dst) ] ++;    
+    cur++;
+
+    //printf("%16lu %16lu\n", src, dst);
+
   }
+
+  printf("Bin Sizes:\n");
+  for(int i = 0 ; i < NUM_BINS; i ++){
+
+    printf("%d\n",bin_sz[i]);
+
+  }
+
 }
 
 int main (int argc, char *argv[]){
---
+++ rand_graph.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <string.h>
+#include <time.h> 
+#include <unistd.h>
+#include <errno.h>
+
+#define V_NAME_LEN 8
+#define NUM_BINS 256
+
+unsigned long num_edges;
+void write_rand_el_file(char *f){
+
+  struct stat stat;
+  int fd = open(f,  O_CREAT | O_RDWR | O_TRUNC, (mode_t) 0x0777);
+  if( fd == -1 ){  
+
+    fprintf(stderr,"bad edgelist file\n");
+    exit(-1); 
+
+  }
+  if(lseek(fd, num_edges * sizeof(unsigned long) * 2 - 1, SEEK_SET) == -1){
+    fprintf(stderr,"seek error\n");
+    exit(-1);
+  }
+ 
+  if(write(fd,"",1) == -1){
+    fprintf(stderr,"write error\n");
+    exit(-1);
+  }
+
+  char *el = 
+    mmap(NULL, num_edges * sizeof(unsigned long) * 2, 
+          PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, fd, 0);
+
+  if( el == MAP_FAILED ){
+    fprintf(stderr,"%s\n",strerror(errno));
+    fprintf(stderr,"bad edgelist map attempt\n");
+    exit(-1);
+  }
+
+  printf ("Generating edge array...");
+  unsigned long *edges = malloc(sizeof(unsigned long) * num_edges * 2);  
+  for(int i = 0; i < num_edges; i += 2){
+    edges[i] = rand(); 
+    edges[i+1] = rand();
+  }
+  printf ("Done.\n");
+  printf ("Copying to file...");
+  memcpy(el, edges, num_edges * sizeof(unsigned long) * 2);
+  printf ("Done.\n");
+  printf ("Syncing file...");
+  msync(el, num_edges * sizeof(unsigned long) * 2, MS_SYNC);
+  printf ("Done.\n");
+  printf ("Unmapping file...");
+  munmap(el, num_edges * sizeof(unsigned long) * 2);
+  printf ("Done.\n");
+  close(fd);
+
+}
+
+int main (int argc, char *argv[]){
+
+  srand( time(NULL) );
+
+  if(argc != 3){ 
+    fprintf(stderr,"rand_graph <outfile> <numedges>\n"); 
+    exit(-1); 
+  }
+
+  num_edges = atol(argv[2]);
+  printf("Generating %lu edges\n",num_edges);
+  write_rand_el_file(argv[1]);
+
+}
