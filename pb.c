d977aa3bfc2edde5e339326c99fe2d0c7479e6f0
Brandon Lucia <blucia@gmail.com>
2020-04-20 11:39:26 -0400

    various changes debugging the graph generator

--- pb.c
+++ pb.c
@@ -10,8 +10,17 @@
 #define NUM_BINS 8
 
 #define v2bin(x) (x % NUM_BINS)
-int bin_sz[NUM_BINS];
+
+typedef unsigned long vertex_t;
+typedef struct edge {
+  vertex_t src;
+  vertex_t dst;
+} edge_t;
+
 int num_edges;
+int bin_sz[NUM_BINS];
+vertex_t *bins[NUM_BINS];
+
 char *init_el_file(char *f){
 
   struct stat stat;
@@ -25,10 +34,10 @@ char *init_el_file(char *f){
 
   fstat(fd,&stat);
   int sz = stat.st_size;
-  num_edges = sz / V_NAME_LEN / 2;
+  num_edges = sz / (sizeof(vertex_t) * 2);
+  fprintf(stderr,"Loading %d edges\n",num_edges);
   char *el = 
-    mmap(NULL, sz, PROT_READ, 
-                             MAP_PRIVATE /*| MAP_HUGETLB | MAP_HUGE_1GB*/, fd, 0);
+    mmap(NULL, sz, PROT_READ, MAP_PRIVATE /*| MAP_HUGETLB | MAP_HUGE_1GB*/, fd, 0);
   if( el == MAP_FAILED ){
     fprintf(stderr,"bad edgelist map attempt\n");
   }
@@ -38,18 +47,17 @@ char *init_el_file(char *f){
 
 void bin_info(char *el){
 
-  unsigned long *cur = (unsigned long *)el;
+  vertex_t *cur = (vertex_t *)el;
   for(int i = 0; i < num_edges; i++){ 
 
-    unsigned long src = *cur;
-    bin_sz[ v2bin(src) ] ++;    
+    vertex_t src = *cur;
+    (bin_sz[ src % NUM_BINS ]) ++;    
+    cur++; 
+
+    vertex_t dst = *cur;
     cur++;
 
-    unsigned long dst = *cur;
-    bin_sz[ v2bin(dst) ] ++;    
-    cur++;
-
-    //printf("%16lu %16lu\n", src, dst);
+    fprintf(stderr,"(%lu,%lu)\n",src,dst);
 
   }
 
@@ -57,6 +65,7 @@ void bin_info(char *el){
   for(int i = 0 ; i < NUM_BINS; i ++){
 
     printf("%d\n",bin_sz[i]);
+    //bins[i] = malloc(sizeof(vertex_t)
 
   }
 
@@ -66,5 +75,6 @@ int main (int argc, char *argv[]){
 
   char *el = init_el_file(argv[1]);
   bin_info(el);
+  
 
 }
