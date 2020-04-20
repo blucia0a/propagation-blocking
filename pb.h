ffa4d839973bf7ebadbf7c9c69af816e53ae9f8d
Brandon Lucia <blucia@gmail.com>
2020-04-20 14:04:55 -0400

    added some things in a header that makes it easier to think about

---
+++ pb.h
@@ -0,0 +1,20 @@
+#define MAX_VTX 10000000
+#define V_NAME_LEN 8
+#define NUM_BINS 256
+
+#define v2bin(x) (x % NUM_BINS)
+#define e2bin(x,y) (x % NUM_BINS)
+#define e2key(x,y) (x)
+#define e2val(x,y) (y)
+
+typedef unsigned long vertex_t;
+typedef unsigned long val_t;
+typedef struct edge {
+  vertex_t src;
+  vertex_t dst;
+} edge_t;
+
+typedef struct bin_elem{
+  vertex_t key;
+  val_t val;
+} bin_elem_t;
